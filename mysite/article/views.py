623b5684ec498fa552e2b6db763c0a3f834bcb27
wujialiang999 <1647113057@qq.com>
2019-12-06 12:52:40 +0800

    创建Article app的article_detail函数,显示文章id

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def article_detail(request,article_id):
+    return HttpResponse("文章id为：{}".format(article_id))
