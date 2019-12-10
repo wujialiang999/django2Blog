fdebe07cb8a61f011439acf5eac7acdfbc2d73a6
wujialiang999 <1647113057@qq.com>
2019-12-10 11:20:10 +0800

    显示文章标题

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 # Create your views here.
 
 def article_detail(request,article_id):
-    return HttpResponse("文章id为：{}".format(article_id))
+    article = Article.objects.get(id=article_id)
+    return HttpResponse("文章id为：{}，标题为{}".format(article.article_id,article.title))
