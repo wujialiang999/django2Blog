1b010748e7540f7a2b8b41106034173fb79bd4b7
wujialiang999 <1647113057@qq.com>
2019-12-10 21:01:13 +0800

    修改模型，增加创建时间字段

--- mysite/article/models.py
+++ mysite/article/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=30) #标题
     content = models.TextField()# 内容
+    created_time=models.DateTimeField(auto_now_add=True)# 创建时间
     def __str__(self):
         return "Article:{}".format(self.title)
