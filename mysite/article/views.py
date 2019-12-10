252bd6ec30123a06aa817e9303aed52afdeeb282
wujialiang999 <1647113057@qq.com>
2019-12-10 11:26:29 +0800

    Http404捕获异常

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from .models import Article
 # Create your views here.
 
@@ -7,5 +7,5 @@ def article_detail(request,article_id):
     try:
         article=Article.objects.get(id=article_id)
     except Article.DoesNotExist:
-        return HttpResponse("页面不存在!")
+        raise Http404("页面不存在!")
     return HttpResponse("文章id为：{},标题:{},内容:{}".format(article_id,article.title,article.content))
